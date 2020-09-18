60e2ea77d6a49e64f31db8eaa82d2ca1c6053d9a
lujdong <zhouyuan8797@gmail.com>
2020-09-18 15:33:36 +0800

    根据hash滚动到指定位置

--- blog/js/reset.js
+++ blog/js/reset.js
@@ -1,3 +1,15 @@
-$('#card-info-btn').click(() => {
-    btf.snackbarShow('使用快捷键 ctrl+D 将本站加入到书签！')
+$(() => {
+    $('#card-info-btn').click(() => {
+        btf.snackbarShow('使用快捷键 ctrl+D 将本站加入到书签！')
+    })
+
+    // 滚动到评论处
+    if (window.location.hash) {
+        var checkExist = setInterval(function () {
+            if ($(window.location.hash).length) {
+                $('html, body').animate({ scrollTop: $(window.location.hash).offset().top - 90 }, 1000)
+                clearInterval(checkExist)
+            }
+        }, 100)
+    }
 })
